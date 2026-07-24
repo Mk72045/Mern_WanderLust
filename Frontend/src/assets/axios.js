ed836deed25c04bc9148d07093183ee42e35e765
Manoj Kumar <manoj53739@gmail.com>
2026-07-24 17:48:55 +0530

    vite url chages

--- Frontend/src/assets/axios.js
+++ Frontend/src/assets/axios.js
@@ -1,8 +1,13 @@
 // api.js
 import axios from "axios";
 
+const PRODUCTION_API_URL = "https://mern-wanderlust-4i7c.onrender.com";
+const DEVELOPMENT_API_URL = "http://localhost:8080";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    (import.meta.env.PROD ? PRODUCTION_API_URL : DEVELOPMENT_API_URL),
 });
 
 export default api;
